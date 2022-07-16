6fa1c1e86dc267ed5339bf2673644f2c1f8ba88b
marciogalvao89 <32371159+marciogalvao89@users.noreply.github.com>
2022-07-16 15:03:00 +0100

    a
    a

--- config.lua
+++ config.lua
@@ -2,26 +2,26 @@ QB = {}
 
 QB.Spawns = {
     ["legion"] = {
-        coords = vector4(195.17, -933.77, 29.7, 144.5),
+        coords = vector4(233.73, -791.18, 30.63, 144.5),
         location = "legion",
-        label = "Legion Square",
+        label = "Safe Zone 1",
     },
 
     ["policedp"] = {
-        coords = vector4(428.23, -984.28, 29.76, 3.5),
+        coords = vector4(-468.23, 6000.28, 31.39, 3.5),
         location = "policedp",
-        label = "Police Department",
+        label = "Safe Zone 2",
     },
 
     ["paleto"] = {
-        coords = vector4(80.35, 6424.12, 31.67, 45.5),
+        coords = vector4(-83.76, -871.18, 41.5, 45.5),
         location = "paleto",
-        label = "Paleto Bay",
+        label = "Safe Zone 3",
     },
 
     ["motel"] = {
-        coords = vector4(327.56, -205.08, 53.08, 163.5),
+        coords = vector4(1569.55, 3593.18, 35.5, 163.5),
         location = "motel",
-        label = "Motels",
+        label = "Safe Zone 4",
     },
 }
