ed08106935b213671d0aa288049f5fa3981fa13c
marciogalvao89 <32371159+marciogalvao89@users.noreply.github.com>
2023-09-27 23:17:44 +0100

    1
    1

---
+++ locales/pt.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    ui = {
+        last_location = "Última localização",
+        confirm = "Confirmar",
+        where_would_you_like_to_start = "Onde queres começar?",
+    }
+}
+if GetConvar('qb_locale', 'en') == 'pt' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
